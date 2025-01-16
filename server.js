404007cc5c050fa368ab82be0f99bfb0ba54176b
Nathalya <nathalya.gsn@gmail.com>
2025-01-16 12:00:53 -0300

    mongodb add

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
 const users = []
 
 app.post('/usuarios', (req, res)=>{
-    
+    users.push(req.body)
+
+    res.status(201).json(req.body)
 })
 
 app.get('/usuarios', (req, res) => {
-    res.send("Enviado com sucesso.")
+    res.status(200).json(users)
 })
 
 app.listen(3000)
