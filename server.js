e4db4013a2cb6ec2004c72f094ab1aaad04e1870
Nathalya <nathalya.gsn@gmail.com>
2025-01-16 16:46:33 -0300

    query add email age

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.get('/usuarios', async(req, res) => {
     if (req.query) {
         users = await prisma.user.findMany({
             where: {
-                name: req.query.name
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age
             }
         })
     } else {
